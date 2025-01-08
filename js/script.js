832e2bc241a2a92b852a20bfd249302661a69c20
Mim Obaidur Rahman Nasim <obaidur150@gmail.com>
2025-01-08 12:54:59 +0600

    Updated few changes

--- js/script.js
+++ js/script.js
@@ -1,40 +1,49 @@
-// toggle icon bar 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// toggle icon bar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
-// scroll section 
-let section  = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+// scroll section
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    section.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height) {
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
 
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
+      // active section for animation on scroll
+      sec.classList.add("show-animate");
+    }
+  });
 
-            // active section for animation on scroll
-            sec.classList.add('show-animate');
-        }
-    });
+  // sticky header
+  let header = document.querySelector("header");
 
-    // sticky header 
-    let header = document.querySelector('header');
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    header.classList.toggle('sticky', window.scrollY > 100);
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
 
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-}
+// Reset the form when the page is loaded
+window.addEventListener("load", function () {
+  const contactForm = document.getElementById("contact-form");
+  if (contactForm) {
+    contactForm.reset();
+  }
+});
